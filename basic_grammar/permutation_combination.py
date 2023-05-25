d82ae36823172acbe0e199a7569e393cc6da419a
Junil Huh <hji10141@naver.com>
2023-05-25 19:01:13 +0900

    Add files via upload

---
+++ basic_grammar/permutation_combination.py
@@ -0,0 +1,24 @@
+"""
+
+[permutation 또는 combination으로 튜플 만들기]
+(사용법)
+import itertools
+a = [1, 2, 3, 4]
+for i in itertools.permutations(a, 3):      # permutation(a, 3) -> 4P3
+    실행문
+for i in itertools.combinations(a, 3):      # combinations(a, 3) -> 4C3
+    실행문
+
+"""
+
+# permutation
+import itertools
+a = [1, 2, 3, 4]
+for i in itertools.permutations(a, 3):      # permutation(a, 3) -> 4P3
+    print(i)
+
+# combination
+import itertools
+a = [1, 2, 3, 4]
+for i in itertools.combinations(a, 3):      # combinations(a, 3) -> 4C3
+    print(i)
